bf2396e8871c7f68788a97f7e7f10c2525262ce6
duchnuon <duchnuon@gmail.com>
2026-01-22 17:55:31 +0700

    update reset password

--- e2e/otp/forgetPassword.spec.ts
+++ e2e/otp/forgetPassword.spec.ts
@@ -22,7 +22,37 @@ test.describe('Forget Password (OTP)',
     });
 
     test('to get link and reset password', async ({ page }) => {
+
         await page.goto(process.env.BASE_MAIL_URL || '', { waitUntil: 'networkidle', timeout: 90000 });
+
+        await expect(page.getByText('OrangeHRM Password Reset').first()).toBeVisible();
+
+        await expect(page.getByText('a few seconds ago').first()).toBeVisible();
+        await page.getByText('OrangeHRM Password Reset').first().click();
+
+        await expect(page.locator('#preview-html').contentFrame().locator('body')).toContainText('auth/resetPassword/resetCode/');
+
+        const frame = await page.locator('#preview-html').contentFrame();
+        // Get all text content
+        const emailText = await frame.locator('body').innerText();
+
+        // // Extract the URL using regex
+        const resetLink = emailText.match(/http[s]?:\/\/[^\s]+/) ?? [];
+
+        await page.goto(resetLink[0] as string, { waitUntil: 'networkidle', timeout: 90000 });
+
+        await expect(page.getByRole('heading', { name: 'Reset Password' })).toBeVisible();
+
+        await page.locator('input[name="password"]').fill('123456+-*/@Abc');
+        await page.locator('input[name="confirmPassword"]').fill('123456+-*/@Abc');
+
+        await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
+
+        await page.getByRole('button', { name: 'Save' }).click();
+
+        await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+        await page.close();
     });
     
 });
